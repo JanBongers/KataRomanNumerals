d39e2ae299715f0ee9d0dda82e7b086b9aa6af66
JanBongers <jan.flip@smartinstall.nl>
2018-11-11 17:54:56 +0100

    Formatting en cleaning

--- src/nl/bongers/RomanNumeralsConverter.java
+++ src/nl/bongers/RomanNumeralsConverter.java
@@ -3,17 +3,17 @@ package nl.bongers;
 public class RomanNumeralsConverter {
 
     private StringBuilder builder;
-    private int[] numbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private int[] numbers = {1_000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     private String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
     public String numberToRoman(int number) {
-        if(number >= 4000) return "Input exceeds maximum of 4000";
+        if (number >= 4_000) return "Input exceeds 4000";
 
         int remaining = number;
         builder = new StringBuilder();
 
-        while(remaining > 0) {
-            for(int i = 0; i < numbers.length; i++) {
+        while (remaining > 0) {
+            for (int i = 0; i < numbers.length; i++) {
                 remaining = append(remaining, numbers[i], romans[i]);
             }
         }
--- test/nl/bongers/RomanNumeralsConverterTest.java
+++ test/nl/bongers/RomanNumeralsConverterTest.java
@@ -18,7 +18,7 @@ public class RomanNumeralsConverterTest {
     public void convertSingleDigitToRomanNumeral() {
         int[] singleDigits = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         String[] expectedRomans = {"I","II","III","IV","V","VI","VII","VIII","IX"};
-        for(int i = 0; i < singleDigits.length; i++) {
+        for (int i = 0; i < singleDigits.length; i++) {
             assertEquals(expectedRomans[i], converter.numberToRoman(singleDigits[i]));
         }
     }
@@ -27,7 +27,7 @@ public class RomanNumeralsConverterTest {
     public void convertDoubleDigitsToRomanNumeral() {
         int[] doubleDigit = {10, 12, 14, 15, 19, 20, 30, 40, 44, 45, 50, 60, 70, 80, 90};
         String[] expectedRomans = {"X","XII","XIV","XV","XIX","XX","XXX","XL","XLIV","XLV","L","LX","LXX","LXXX","XC"};
-        for(int i = 0; i < doubleDigit.length; i++) {
+        for (int i = 0; i < doubleDigit.length; i++) {
             assertEquals(expectedRomans[i], converter.numberToRoman(doubleDigit[i]));
         }
     }
@@ -36,7 +36,7 @@ public class RomanNumeralsConverterTest {
     public void convertTripleDigitsToRomanNumeral() {
         int[] tripleDigits = {100, 200, 300, 400, 500, 600, 700, 800, 900, 999};
         String[] expectedRomans = {"C","CC","CCC","CD","D","DC","DCC","DCCC","CM","CMXCIX"};
-        for(int i = 0; i < tripleDigits.length; i++) {
+        for (int i = 0; i < tripleDigits.length; i++) {
             assertEquals(expectedRomans[i], converter.numberToRoman(tripleDigits[i]));
         }
     }
@@ -45,13 +45,13 @@ public class RomanNumeralsConverterTest {
     public void convertQuadrupleDigitsToRomalNumeral() {
         int[] quadrupleDigits = {1000, 2000, 3000, 3999};
         String[] expectedRomans = {"M","MM","MMM","MMMCMXCIX"};
-        for(int i = 0; i < quadrupleDigits.length; i++) {
+        for (int i = 0; i < quadrupleDigits.length; i++) {
             assertEquals(expectedRomans[i], converter.numberToRoman(quadrupleDigits[i]));
         }
     }
 
     @Test
     public void numberToHigh() {
-        assertEquals("Input exceeds maximum of 4000", converter.numberToRoman(4000));
+        assertEquals("Input exceeds 4000", converter.numberToRoman(4000));
     }
 }
